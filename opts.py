8f071ea6da6cb70f91e7cfcc0c658af5d5b866c4
mizuhof <mizuhof0@gmail.com>
2020-08-24 17:43:08 +0900

    change args default values

--- opts.py
+++ opts.py
@@ -7,7 +7,7 @@ def argparser():
 
     parser.add_argument('--log_path', type=str, default="log")
     parser.add_argument('--device_ids', type=str, default="")
-    parser.add_argument('--epochs', default=50, type=int, metavar='N',
+    parser.add_argument('--epochs', default=100, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('-b', '--batch_size', default=128, type=int,
                         metavar='N', help='mini-batch size (default: 256)')
@@ -17,7 +17,7 @@ def argparser():
                         help='momentum')
     parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
                         metavar='W', help='weight decay (default: 5e-4)')
-    parser.add_argument('--eval_freq', default=5, type=int,
+    parser.add_argument('--eval_freq', default=100, type=int,
                         help='number of evalation frequency')
     parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
                         help='number of data loading workers (default: 8)')
