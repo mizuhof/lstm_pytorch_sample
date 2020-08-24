0f8d4994b3fd223c89cadf991a686ea8e311898c
mizuhof <mizuhof0@gmail.com>
2020-08-24 17:48:02 +0900

    fix foward. change hidden size

--- model.py
+++ model.py
@@ -5,19 +5,20 @@ import torch.nn as nn
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.lstm1 = nn.LSTMCell(2, 256)
-        self.lstm2 = nn.LSTMCell(256, 256)
-        self.linear = nn.Linear(256, 2)
+        self.lstm1 = nn.LSTMCell(2, 512)
+        self.lstm2 = nn.LSTMCell(512, 512)
+        self.linear = nn.Linear(512, 2)
+        self.drop = nn.Dropout()
 
     def forward(self, inputs, future=0):
         """
         inputs: (batch, seq_len, 2)
         """
         device = inputs.device
-        h_t = torch.zeros(inputs.size(0), 256, dtype=torch.float).to(device)
-        c_t = torch.zeros(inputs.size(0), 256, dtype=torch.float).to(device)
-        h_t2 = torch.zeros(inputs.size(0), 256, dtype=torch.float).to(device)
-        c_t2 = torch.zeros(inputs.size(0), 256, dtype=torch.float).to(device)
+        h_t = torch.zeros(inputs.size(0),  512, dtype=torch.float).to(device)
+        c_t = torch.zeros(inputs.size(0),  512, dtype=torch.float).to(device)
+        h_t2 = torch.zeros(inputs.size(0), 512, dtype=torch.float).to(device)
+        c_t2 = torch.zeros(inputs.size(0), 512, dtype=torch.float).to(device)
 
         inputs = inputs.permute(1, 0, 2)
         outputs = [torch.zeros_like(inputs[0])]  # 処理の都合上のダミーを入れておく
@@ -25,8 +26,9 @@ class Model(nn.Module):
             lstm_input_t = input_t.clone()
             lstm_input_t[input_t == 0] = outputs[i][input_t == 0]
 
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t, c_t = self.lstm1(lstm_input_t, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+
             output = self.linear(h_t2)
             outputs += [output]
 
